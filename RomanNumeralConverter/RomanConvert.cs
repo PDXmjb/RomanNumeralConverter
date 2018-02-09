3b85e369cab84bce9c2bba428484b56bf71041e6
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-02-08 16:06:37 -0800

    Converts given integer to its roman numeral equivalent. Note than the integer has to be > 0 and <= 3999.

---
+++ RomanNumeralConverter/RomanConvert.cs
@@ -0,0 +1,33 @@
+﻿namespace RomanNumeralConverter
+{
+    using System;
+    using System.Linq;
+
+    public class RomanConvert
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(3999));
+            Console.ReadKey();
+        }
+
+        public static string Solution(int n)
+        {
+            var romanNum = new string[][] {new string[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+                                           new string[] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+                                           new string[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+                                           new string[] {"", "M", "MM", "MMM"}};
+
+            // Pulls out the individual digits, starting with the ONES place.
+            var nums = n.ToString().ToCharArray().Reverse().Select(c => byte.Parse(c.ToString())).ToArray();
+            var roman = String.Empty;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                roman = String.Concat(romanNum[i][nums[i]], roman);
+            }
+
+            return roman;
+        }
+    }
+}
---
+++ RomanNumeralConverter/RomanConvertTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using RomanNumeralConverter;
+
+[TestFixture]
+public class RomanConvertTests
+{
+    [TestCase(1, "I")]
+    public void Test1(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(3, "III")]
+    public void Test3(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(4, "IV")]
+    public void Test4(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(5, "V")]
+    public void Test5(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(9, "IX")]
+    public void Test9(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(2018, "MMXVIII")]
+    public void Test2018(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(889, "DCCCLXXXIX")]
+    public void Test889(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+
+    [TestCase(3999, "MMMCMXCIX")]
+    public void Test3999(int value, string expected)
+    {
+        Assert.AreEqual(expected, RomanConvert.Solution(value));
+    }
+}
